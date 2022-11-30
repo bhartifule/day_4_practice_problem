4dafa74d37446f138fcd0dc77d8dd26d6a54f331
Bharti <bharatifule10gmail.com>
2022-11-30 23:39:58 +0530

    play rolls the die added

--- bridgelabz/SnakeAndLadder.java
+++ bridgelabz/SnakeAndLadder.java
@@ -4,6 +4,8 @@ public class SnakeAndLadder {
 	public static void main(String[] args) {
 		int playerPosition = 0;
 		System.out.println("Position of player is " + playerPosition);
-	
+		// To Get Number Between 1-6
+		int randomCheck = (int) Math.floor(Math.random() * 10) % 6 + 1;
+		System.out.println(randomCheck);
 }
 }
