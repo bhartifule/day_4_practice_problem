59ea1b62f02508c5b02a96f898555465e064eda2
Bharti <bharatifule10gmail.com>
2022-11-30 23:50:14 +0530

    winning position added

--- bridgelabz/SnakeAndLadder.java
+++ bridgelabz/SnakeAndLadder.java
@@ -2,32 +2,43 @@ package com.bridgelabz;
 
 public class SnakeAndLadder {
 	public static void main(String[] args) {
+		System.out.println("Player initial Position is: ");
+
 		int position = 0;
 
-        int dice = (int) Math.floor(Math.random() * 10) % 6 + 1;
-        System.out.println("DiceNumber is : " + dice);
+		while (position < 100) {
 
-        int option = (int) Math.floor(Math.random() * 10) % 3;
-        System.out.print("Player option is : ");
+			int dice = (int) Math.floor(Math.random() * 10) % 6 + 1;
+			System.out.println("diceNumber is : " + dice);
 
-        switch (option) {
-            case 0:
-                position += dice;
-                System.out.println("Ladder");
-                System.out.println("Position of the player after the ladder is : " + position);
-                break;
+			int option = (int) Math.floor(Math.random() * 10) % 3;
+			System.out.println("selected player option : " + option);
 
-            case 1:
-                position -=  dice;
-                System.out.println("Snake");
-                System.out.println("Position of the player after the Snake is : " + position);
-                break;
+			switch (option) {
+			case 0:
+				position += dice;
+				System.out.println("Ladder");
+				System.out.println("position for the player after the ladder is : " + position);
+				break;
 
-            case 2:
-                System.out.println("No play");
-                System.out.println("Position of the player remains same");
-                break;
-        }
+			case 1:
+				position -= dice;
+				System.out.println("Snake");
+				if (position < 0) {
+					position = 0;
+					System.out.println("Player restart from zero: ");
+				}
+				System.out.println("position for the player after the Snake is : " + position);
+				break;
+
+			case 2:
+				System.out.println("No play");
+				System.out.println("Position of the player remains same");
+				break;
+			}
+		}
+
+	
 
 	}
 }
