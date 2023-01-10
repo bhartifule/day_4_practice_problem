60fd3de73d71c190e6e1f25723979cd2c521f29b
bhartifule <116824470+bhartifule@users.noreply.github.com>
2023-01-10 15:24:05 +0530

    Added:Snake And Ladder Problems

--- bridgelabz/SnakeAndLadder.java
+++ bridgelabz/SnakeAndLadder.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.Day4;
 
-public class SnakeAndLadder {
+public class SnakeAndLadder 
+{
 	public static void main(String[] args) {
 		System.out.println("Player initial Position is: ");
 
