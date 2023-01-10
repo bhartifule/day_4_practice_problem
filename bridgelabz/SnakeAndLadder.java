63aae0f54426ec77fbc2b19d989da720a876969a
bhartifule <116824470+bhartifule@users.noreply.github.com>
2023-01-10 16:22:42 +0530

    Added:Snake And Ladder Problem

--- bridgelabz/SnakeAndLadder.java
+++ bridgelabz/SnakeAndLadder.java
@@ -2,6 +2,6 @@ package com.bridgelabz.Day4;
 
 public class SnakeAndLadder {
 public static void main(String[] args) {
-	System.out.println("welcome to snake and ladder");
+	System.out.println("welcome to snake and ladder program");
 }
 }
