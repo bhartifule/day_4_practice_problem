08d2d14e4a1d4e74b61c194131fd1d9d4c90c81f
bhartifule <116824470+bhartifule@users.noreply.github.com>
2023-01-10 14:04:54 +0530

    Added:Snake And Ladder Problem

--- bridgelabz/SnakeAndLadder.java
+++ bridgelabz/SnakeAndLadder.java
@@ -3,7 +3,7 @@ package com.bridgelabz.Day4;
 public class SnakeAndLadder {
 	public static void main(String[] args) {
 		int playerPosition = 0;
-		System.out.println("Position of player is " + playerPosition);
+		System.out.println("Position of player is" + playerPosition);
 	
 }
 }
